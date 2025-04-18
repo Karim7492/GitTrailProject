79aaa5c7cd66dc0affbc079448eb156339b6dcab
Karim7492 <abdulkarimazad74@gmail.com>
2025-04-18 23:08:45 +0530

    GitTraill

---
+++ src/org/git/GitTrail.java
@@ -0,0 +1,17 @@
+package org.git;
+
+public class GitTrail {
+	
+	public void m1() {
+		
+		System.out.println("Welcome");
+
+	}
+	public static void main(String[] args) {
+		
+		GitTrail g = new GitTrail();
+		g.m1();
+		
+	}
+
+}
